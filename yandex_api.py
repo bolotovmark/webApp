f6fd9e3f4b6ef124e2a20154dcc6793e2a612c90
Ekaterina <92542464+Ekaterina-Tyutyunykh@users.noreply.github.com>
2022-11-09 09:11:22 +0500

    first response

--- yandex_api.py
+++ yandex_api.py
@@ -1,3 +1,12 @@
-def parse_weather():
+import requests, json
 
-    print("api")
+
+def parse_weather():
+    # утро - 5:00 день - 11 вечер 17 ночь 23
+    # характер погоды : осадки и облачки
+    # температура + - 0
+    # ветер м/c
+    url = 'https://api.weather.yandex.ru/v2/forecast?lat=55.75396&lon=37.620393&lang=ru_RU&limit=1&hours=true&extra=true'
+    response = requests.get(url, headers={'X-Yandex-API-Key': '25953e0d-5412-41e2-885a-0db6e929f689'})
+    print(response.json())
+
