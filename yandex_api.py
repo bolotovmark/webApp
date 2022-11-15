f5a78e81ce2452a605cd8f5373eedabdc77f59c5
Ekaterina <92542464+Ekaterina-Tyutyunykh@users.noreply.github.com>
2022-11-15 22:54:19 +0500

    modify script to array

--- yandex_api.py
+++ yandex_api.py
@@ -9,30 +9,36 @@ def parse_weather():
     url = 'https://api.weather.yandex.ru/v2/forecast?lat=58.010000&lon=56.229143&lang=ru_RU&limit=2&hours=true&extra=true'
     response = requests.get(url, headers={'X-Yandex-API-Key': '25953e0d-5412-41e2-885a-0db6e929f689'}).json()
     print(response)
+    count = 1
     for part in response['forecasts']:
-        day_condition = part['parts']['day']['condition']
-        day_cloudness = part['parts']['day']['cloudness']
-        day_temp = part['parts']['day']['temp_avg']
-        day_wind = part['parts']['day']['wind_speed']
-        #
-        night_condition = part['parts']['night']['condition']
-        night_cloudness = part['parts']['night']['cloudness']
-        night_temp = part['parts']['night']['temp_avg']
-        night_wind = part['parts']['night']['wind_speed']
-        #
-        morning_condition = part['parts']['morning']['condition']
-        morning_cloudness = part['parts']['morning']['cloudness']
-        morning_temp = part['parts']['morning']['temp_avg']
-        morning_wind = part['parts']['morning']['wind_speed']
-        #
-        evening_condition = part['parts']['evening']['condition']
-        evening_cloudness = part['parts']['evening']['cloudness']
-        evening_temp = part['parts']['evening']['temp_avg']
-        evening_wind = part['parts']['evening']['wind_speed']
-        print(morning_condition, morning_cloudness, morning_temp, morning_wind)
-        print(day_condition, day_cloudness, day_temp, day_wind)
-        print(evening_condition, evening_cloudness, evening_temp, evening_wind)
-        print(night_condition, night_cloudness, night_temp, night_wind)
+        if count == 1:
+            count = 2
+        else:
+            day_condition = part['parts']['day']['condition']
+            day_cloudness = part['parts']['day']['cloudness']
+            day_temp = part['parts']['day']['temp_avg']
+            day_wind = part['parts']['day']['wind_speed']
+            #
+            night_condition = part['parts']['night']['condition']
+            night_cloudness = part['parts']['night']['cloudness']
+            night_temp = part['parts']['night']['temp_avg']
+            night_wind = part['parts']['night']['wind_speed']
+            #
+            morning_condition = part['parts']['morning']['condition']
+            morning_cloudness = part['parts']['morning']['cloudness']
+            morning_temp = part['parts']['morning']['temp_avg']
+            morning_wind = part['parts']['morning']['wind_speed']
+            #
+            evening_condition = part['parts']['evening']['condition']
+            evening_cloudness = part['parts']['evening']['cloudness']
+            evening_temp = part['parts']['evening']['temp_avg']
+            evening_wind = part['parts']['evening']['wind_speed']
+
+            mas_weather = [[morning_condition, morning_cloudness, morning_temp, morning_wind],[day_condition, day_cloudness, day_temp, day_wind],[evening_condition, evening_cloudness, evening_temp, evening_wind],[night_condition, night_cloudness, night_temp, night_wind]]
+            for row in mas_weather:
+                for elem in row:
+                    print(elem, end=' ')
+                print()
 
 
 
