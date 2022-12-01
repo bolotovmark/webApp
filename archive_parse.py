f0d935ec91322368d0f2b79f4a223a98f408dedc
bolotovmark <92545761+bolotovmark@users.noreply.github.com>
2022-12-01 22:59:48 +0500

    update archive parse

--- archive_parse.py
+++ archive_parse.py
@@ -3,13 +3,25 @@ from bs4 import BeautifulSoup
 import re
 
 def get_archive():
-    url = 'http://www.hmn.ru/index.php?index=8&value=28224'
+    url = 'http://www.pogodaiklimat.ru/weather.php?id=28224'
+    headers = requests.utils.default_headers()
+
+    headers.update(
+        {
+            'User-Agent': 'My User Agent 1.0',
+        }
+    )
     response = requests.get(url)
+    response.encoding = "unf-8"
     soup = BeautifulSoup(response.text, 'lxml')
-    table = soup.find('table', class_="m80")
+    table = soup.find('div', class_="archive-table-wrap")
     tr = table.findAll('tr')
-    for i in {1, 3, 5, 7}:
-        td = tr[i].findAll('td')
-        print(tr[i])
+    for i in {-7, -5, -3, -1}:
+        #print(tr[i], "\n--------------\n")
+        td = tr[i].find_all('td')
+        print(td[1].text)
+        print(td[3].text)
+        print(td[5].text)
+
 
 
