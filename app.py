16731a230802c81a48ce808259060fcd5965dae1
bolotovmark <92545761+bolotovmark@users.noreply.github.com>
2022-12-02 12:19:17 +0500

    add scheduled module

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask_apscheduler import APScheduler
 
 from parse.archive_parse import get_archive
 from parse.gismeteo import get_gis
@@ -6,17 +7,19 @@ from parse.yandex_api import parse_weather
 from parse.rp5 import get_bs
 
 app = Flask(__name__)
-
+scheduler = APScheduler()
+scheduler.init_app(app)
+scheduler.start()
 
 @app.route('/')
 def hello_world():  # put application's code here
     return render_template('index.html')
 
 
-if __name__ == '__main__':
+@scheduler.task('interval', id='do_job_1', seconds=5)
+def scheduled_task():
     get_archive()
-    get_gis()
-    parse_weather()
-    get_bs()
 
+
+if __name__ == '__main__':
     app.run()
